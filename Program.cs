5f01d928a51b3c136c3e6ada13ae37f07085f7cb
FreezaCaipira <robercraudio@gmail.com>
2026-07-12 11:47:51 -0300

    Fase 3: Google OAuth pacote + configuração backend

--- Program.cs
+++ Program.cs
@@ -22,6 +22,7 @@ using Confirmai.Configuration;
 using Confirmai.Hubs;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Components.Authorization;
 using System.Threading.RateLimiting;
 using Microsoft.AspNetCore.RateLimiting;
@@ -254,8 +255,21 @@ builder.Services.AddHttpClient("AppmaxAuth", (sp, client) =>
         : opts.AuthBaseUrl);
 });
 
-builder.Services.AddAuthentication()
+var googleClientId = builder.Configuration["Authentication:Google:ClientId"];
+var googleClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+
+var authBuilder = builder.Services.AddAuthentication()
     .AddScheme<ApiKeyAuthOptions, ApiKeyAuthHandler>(ApiKeyAuthDefaults.AuthenticationScheme, _ => { });
+
+if (!string.IsNullOrWhiteSpace(googleClientId) && !string.IsNullOrWhiteSpace(googleClientSecret))
+{
+    authBuilder.AddGoogle(options =>
+    {
+        options.ClientId = googleClientId;
+        options.ClientSecret = googleClientSecret;
+        options.CallbackPath = "/signin-google";
+    });
+}
 
 // S-7: Rate limiting
 builder.Services.AddRateLimiter(options =>
